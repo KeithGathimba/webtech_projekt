871ce3f6875fde3e8897b5cdba80ef15d47dd8b0
Keith Gathimba <Keith.Gathimba@Student.HTW-Berlin.de>
2026-01-18 12:48:42 +0100

    M5 v11

--- src/main/java/de/htwberlin/webtech/rest/model/BookDTO.java
+++ src/main/java/de/htwberlin/webtech/rest/model/BookDTO.java
@@ -1,10 +1,18 @@
 package de.htwberlin.webtech.rest.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record BookDTO(
         Long id,
+        @NotBlank(message = "Titel darf nicht leer sein")
         String title,
+
+        @NotBlank(message = "Autor darf nicht leer sein")
         String author,
+
         int releaseYear,
         String status,
-        int rating // <--- NEU
+        int rating
 ) { }
